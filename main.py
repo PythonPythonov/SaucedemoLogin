76620eb1d8ffa1ced03902d23387d57ce10f179b
Silvar <silvar@ya.ru>
2023-06-19 16:36:10 +0300

    Saucedemo.com login version3

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-username = "standard_user"
-password = "secret_sauce"
+USERNAME = "standard_user"
+PASSWORD = "secret_sauce"
+URL = "https://www.saucedemo.com/"
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-# найти поле имени пользователя / электронной почты и отправить само имя пользователя в поле ввода
-driver.find_element(By.ID, "user-name").send_keys(username)
-# найти поле ввода пароля и также вставить пароль
-driver.find_element(By.ID, "password").send_keys(password)
-# нажмите кнопку входа в систему
-driver.find_element(By.ID, "login-button").click()
+def get_driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+
+def open_login_page(driver, our_url):
+    driver.get(our_url)
+
+def get_element_by_id(driver, locator):
+    return driver.find_element(By.ID, locator)
+
+def click_element_by_id(driver, locator):
+    driver.find_element(By.ID, locator).click()
+
+def element_send_keys(driver, locator, key):
+    elem = get_element_by_id(driver, locator)
+    elem.send_keys(key)
+
+def login(driver, name, password):
+    element_send_keys(driver, "user-name", USERNAME)
+    element_send_keys(driver, "password", PASSWORD)
+    click_element_by_id(driver, "login-button")
+
+
+driver = get_driver()
+open_login_page(driver, URL)
+login(driver=driver, name=USERNAME, password=PASSWORD)
+
 
 # ждем завершения состояния готовности
 WebDriverWait(driver=driver, timeout=10).until(
@@ -21,15 +42,10 @@ WebDriverWait(driver=driver, timeout=10).until(
 error_message = "Incorrect username or password."
 # получаем ошибки (если есть)
 errors = driver.find_elements(By.CLASS_NAME, "error")
-# при необходимости распечатать ошибки
-# для e в ошибках:
-#     print(e.text)
 # если мы находим это сообщение об ошибке в составе error, значит вход не выполнен
 if any(error_message in e.text for e in errors):
     print("[!] Login failed")
 else:
     print("[+] Login successful")
 
-driver.close()
-
-print("Mew")
+driver.quit()
